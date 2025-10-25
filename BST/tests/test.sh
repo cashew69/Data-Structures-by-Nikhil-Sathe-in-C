82b3546f544dd3a33761ccba00151d0432eb2da7
Nikhil Sathe <nikhilsathe32@gmail.com>
2025-10-25 16:01:07 +0530

    Test changed

--- BST/tests/test.sh
+++ BST/tests/test.sh
@@ -2,5 +2,5 @@
 
 min=1
 max=99
-../code/a.out -create $(for i in {1..10}; do echo -n "$(( RANDOM % (max - min + 1) + min )) "; done)
+../code/a.out -create $(for i in {1..100}; do echo -n "$(( RANDOM % (max - min + 1) + min )) "; done)
 
