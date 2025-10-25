211ee288d2f09c871a313aed78858d3ce5b590ef
Nikhil Sathe <nikhilsathe32@gmail.com>
2025-10-25 12:57:31 +0530

    Search, Successor, Parent implemented

--- BST/code/main.c
+++ BST/code/main.c
@@ -7,9 +7,10 @@ typedef struct Node
     int value;
     struct Node *left;
     struct Node *right;
+    struct Node *parent;
 } Node;
 
-// Helpers
+// Operations
 Node* insertNode(Node* currentNode, int value) {
     if (currentNode == NULL)
     {
@@ -17,36 +18,23 @@ Node* insertNode(Node* currentNode, int value) {
         newNode->value = value;
         newNode->left = NULL;
         newNode->right = NULL;
+        newNode->parent = NULL;
         return newNode;
     }
     if (value < currentNode->value)
     {
         currentNode->left = insertNode(currentNode->left, value);
+        currentNode->left->parent = currentNode;
     }
     else if (value > currentNode -> value)
     {
         currentNode->right = insertNode(currentNode->right, value);
+        currentNode->right->parent = currentNode;
     }
 
     return currentNode;
 }
 
-/*Node *getFarLeft(Node * currentNode)
-{
-    if (currentNode != NULL)
-    {   
-        if(currentNode->left != NULL)
-        {
-            return getFarLeft(currentNode->left);
-        }
-        return currentNode;
-    }
-
-    return NULL;
-}*/
-
-
-// Operations
 Node * createBST(int arrSize, int *inputArray)
 {
     if (arrSize > 0)
@@ -68,12 +56,62 @@ void inOrderTraversal(Node* node) {
     if (node == NULL)
         return;  // Base case
 
-    // TODO: Visit left subtree first
     inOrderTraversal(node->left);
-    // TODO: Print current node
     printf("%d ", node->value);
-    // TODO: Visit right subtree
     inOrderTraversal(node->right);
+}
+
+Node * search(Node * node, int searchvalue) 
+{
+    if (node == NULL || searchvalue == node->value)
+    {
+        return node;
+    }
+    if (searchvalue < node->value)
+    {
+        return search(node->left, searchvalue);
+    }
+    else
+    {
+        return search(node->right, searchvalue);
+    }
+}
+
+Node * successor(Node * node, int searchvalue) 
+{
+    if (node->right !=NULL)
+    {
+        return getMinimum(node->right);
+    }
+    else 
+    {
+        Node * parent = node->parent;
+        while (parent != NULL && node == parent->right)
+        {
+            node = parent;
+            parent = parent->parent;
+        }
+        return parent;
+
+    }
+}
+
+Node *getMinimum(Node * currentNode)
+{
+   if (currentNode == NULL || currentNode->left == NULL) 
+   {
+    return currentNode;
+   }
+    return getMinimum(currentNode->left);
+}
+
+Node *getMaximum(Node * currentNode)
+{
+   if (currentNode == NULL || currentNode->right == NULL) 
+   {
+    return currentNode;
+   }
+    return getMaximum(currentNode->right);
 }
 
 int main(int argc, char *argv[]) {
@@ -83,6 +121,7 @@ int main(int argc, char *argv[]) {
     {
         printf("Usage: %s <command> [numbers...]\n", argv[0]);
         printf("Commands: -create\n");
+        printf("          -search\n");
         return 1;
     }
 
@@ -102,15 +141,34 @@ int main(int argc, char *argv[]) {
         inputArray[i - 2] = atoi(argv[i]);  // Note: i - 2
         printf("Added %d to inputArray[%d]\n", inputArray[i - 2], i - 2);
     }
-    
+    Node* root = NULL;
     if (strcmp(argv[1], "-create") == 0) 
     {
-    Node* root = createBST(inputArrayLength, inputArray);
+    root = createBST(inputArrayLength, inputArray);
     printf("BST created successfully!\n");
     printf("In-order traversal (sorted): ");
     inOrderTraversal(root);
     printf("\n");
-    }    else 
+    }    
+    else if (root && strcmp(argv[1], "-search") == 0)
+    {
+        Node *searchNode = search(root, inputArray[2]);
+        if (searchNode)
+        {
+            printf("Found Search Value %d\n", searchNode->value);
+        }
+        else
+        {
+            printf("Search Value Not Found\n");
+        }
+
+    }
+    else if (root && strcmp(argv[1], "-minimums") == 0)
+    {
+        Node * minimumNode = minimum(root);
+        printf("Minium Value: %d \n", minimumNode->value);
+    }
+    else 
     {
         printf("Unknown command: %s\n", argv[1]);
     }
